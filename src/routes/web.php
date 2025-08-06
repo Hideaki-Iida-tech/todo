2ea1a0986f0b8c4b04686d8a9e6fdd491b882b71
iida hideaki <hideaki.19790727@gmail.com>
2025-08-06 21:29:25 +0900

    コード修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,13 +20,13 @@ use App\Http\Controllers\CategoryController;
 
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
-Route::patch('/todos/update', [TodoController::class, 'update']);
-Route::delete('todos/delete', [TodoController::class, 'destroy']);
+Route::patch('/todos/{todo_id}', [TodoController::class, 'update']);
+Route::delete('todos/{todo_id}', [TodoController::class, 'destroy']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::patch(
-    '/categories/update',
+    '/categories/{category_id}',
     [CategoryController::class, 'update']
 );
-Route::delete('/categories/delete', [CategoryController::class, 'destroy']);
+Route::delete('/categories/{category_id}', [CategoryController::class, 'destroy']);
 Route::get('/todos/search', [TodoController::class, 'search']);
